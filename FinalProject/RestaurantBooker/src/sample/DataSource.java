8bbd808dc0c03075aa79f8e21b5ac231bdf16b97
ashfaaqdesai <35612981+ashfaaqdesai@users.noreply.github.com>
2018-03-24 18:18:13 -0400

    Restaurant Data File

---
+++ FinalProject/RestaurantBooker/src/sample/DataSource.java
@@ -0,0 +1,15 @@
+package sample
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class DataSource {
+    public static ObservableList<Restaurant> getAllRestaurants(){
+        ObservableList<Restaurant> info = FXCollections.observableArrayList();
+
+        info.add(new Restaurant("Test Name", 50, "123 Test Rd, Oshawa, ON, L1N5V4", "American"));
+        info.add(new Restaurant("Test Name2", 80, "456 Test Rd, Oshawa, ON, L1N5V4", "Indian"));
+
+        return info;
+    }
+}
